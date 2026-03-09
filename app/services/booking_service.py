a61972fb738119765b68e65f336b05f497105d79
sanskritehe <sanskritisharmaa14@gmail.com>
2026-03-09 23:12:43 +0530

    Add update_booking function to booking_service

--- app/services/booking_service.py
+++ app/services/booking_service.py
@@ -1,4 +1,4 @@
-from app.db_client import create_appointment, get_all_appointments,cancel_appointment
+from app.db_client import create_appointment, get_all_appointments, update_appointment, cancel_appointment
 
 
 def book_appointment(data):
@@ -8,6 +8,8 @@ def book_appointment(data):
 def list_appointments():
     return get_all_appointments()
 
+def update_booking(appointment_id: int, data):
+    return update_appointment(appointment_id, data)
 
 def cancel_booking(appointment_id: int):
     return cancel_appointment(appointment_id)
