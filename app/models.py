932007dc6ba30c3beec5b72394eff30bba7b8a96
sanskritehe <sanskritisharmaa14@gmail.com>
2026-03-09 23:12:36 +0530

    Add AppointmentUpdate model for PUT endpoint

--- app/models.py
+++ app/models.py
@@ -4,6 +4,9 @@ class AppointmentCreate(BaseModel):
     user: str
     time: str
 
+class AppointmentUpdate(BaseModel):
+    time: str
+
 class AppointmentResponse(BaseModel):
     id: int
     user: str
