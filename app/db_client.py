3d9a7393e75ea71bbf6ea95301d3eda0f02c45ac
sanskritehe <sanskritisharmaa14@gmail.com>
2026-05-28 00:35:14 +0530

    feat: add app/db_client.py for KAN-22

--- app/db_client.py
+++ app/db_client.py
@@ -1,29 +1,17 @@
 import requests
 from app.config import DB_SERVICE_URL
 
-def create_appointment(data: dict):
-    response = requests.post(
-        f"{DB_SERVICE_URL}/appointments",
-        params=data
-    )
+
+def get_appointment_by_id(appointment_id: int):
+    response = requests.get(f"{DB_SERVICE_URL}/appointments/{appointment_id}")
     response.raise_for_status()
     return response.json()
 
-def get_all_appointments():
-    response = requests.get(f"{DB_SERVICE_URL}/appointments")
-    response.raise_for_status()
-    return response.json()
 
 def update_appointment(appointment_id: int, data: dict):
-    response = requests.put(
+    response = requests.patch(
         f"{DB_SERVICE_URL}/appointments/{appointment_id}",
         json=data
     )
     response.raise_for_status()
-    return response.json()
-
-def cancel_appointment(appointment_id: int):
-    response = requests.delete(
-        f"{DB_SERVICE_URL}/appointments/{appointment_id}"
-    )
     return response.json()
