bf4f83d3a5c9b140d9aaae201a06fc07c36ac1f6
sanskritehe <sanskritisharmaa14@gmail.com>
2026-06-25 20:57:05 +0530

    feat: add app/main.py for KAN-28

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from strawberry.fastapi import GraphQLRouter
 from app.routes import appointments
-from app.graphql_schema import schema
+from app.database import SessionLocal
 
-app = FastAPI(title="Appointment Service")
+app = FastAPI()
 
 app.include_router(appointments.router)
-
-graphql_app = GraphQLRouter(schema)
-app.include_router(graphql_app, prefix="/graphql")
