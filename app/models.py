fb90909df34f8adaf7d3e83154bf8f04375e7f71
Viraj Ved Shankar <142313889+VirajShankar@users.noreply.github.com>
2026-06-29 18:05:34 +0530

    feat: add app/models.py for KAN-15

--- app/models.py
+++ app/models.py
@@ -1,33 +1,11 @@
-from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
-from app.database import Base, engine
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class AppointmentDB(Base):
+class Appointment(Base):
     __tablename__ = "appointments"
-
-    id: int = Column(Integer, primary_key=True, index=True)  # type: ignore
-    user: str = Column(String, index=True)  # type: ignore
-    time: str = Column(String)  # type: ignore
-    status: str = Column(String, default="Scheduled")  # type: ignore
-
-
-# Ensure table exists in SQLite database
-Base.metadata.create_all(bind=engine)
-
-
-class AppointmentCreate(BaseModel):
-    user: str
-    time: str
-
-
-class AppointmentUpdate(BaseModel):
-    time: str | None = None  # Optional field
-    status: str | None = None  # Optional field
-
-
-class AppointmentResponse(BaseModel):
-    id: int
-    user: str
-    time: str
-    status: str
+    id: int = Column(Integer, primary_key=True)  # type: ignore
+    user: str = Column(String)
+    time: str = Column(String)
+    status: str = Column(String)
